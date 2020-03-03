a411540d283db38f4ecdabc5bea34034df536dc6
Mircea <mrbucerzan@gmail.com>
2020-03-03 21:07:36 +0200

    Created github repos ViewModel
    - Implemented the ViewModel which holds observable data for the UI
    - Modeled loading/success/error states for the UI
    
    Trello: https://trello.com/c/mxW6GCu7

--- app/src/main/java/com/mircea/repobrowser/data/RepoDto.kt
+++ app/src/main/java/com/mircea/repobrowser/data/RepoDto.kt
@@ -6,12 +6,12 @@ import com.google.gson.annotations.SerializedName
  * Model class for a GitHub repository.
  */
 data class RepoDto(
-    @SerializedName("name") val name: String,
-    @SerializedName("description") val description: String,
-    @SerializedName("owner") val owner: OwnerDto
+    @SerializedName("name") val name: String?,
+    @SerializedName("description") val description: String?,
+    @SerializedName("owner") val owner: OwnerDto?
 )
 
 data class OwnerDto(
-    @SerializedName("avatar_url") val avatarUrl: String,
-    @SerializedName("html_url") val htmlUrl: String
+    @SerializedName("avatar_url") val avatarUrl: String?,
+    @SerializedName("html_url") val htmlUrl: String?
 )
