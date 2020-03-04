d4edd2bf35d2216b12132d31d9f1b31ee8450b05
Mircea Bucerzan <mircea@Mirceas-MacBook-Pro.local>
2020-03-04 20:04:54 +0200

    Open repo web page on click
    - Clicking on a list item results in launching an implicit Intent
      to open the browser, displaying the GitHub repo's page
    - The navigation event is controlled by the ViewModel
    
    Trello: https://trello.com/c/mxW6GCu7

--- app/src/main/java/com/mircea/repobrowser/presentation/RepoItem.kt
+++ app/src/main/java/com/mircea/repobrowser/presentation/RepoItem.kt
@@ -4,8 +4,8 @@ package com.mircea.repobrowser.presentation
  * Ui model for a GitHub repository.
  */
 data class RepoItem(
+    val id: Long,
     val name: String,
     val description: String,
-    val imageUrl: String,
-    val githubUrl: String
+    val imageUrl: String
 )
